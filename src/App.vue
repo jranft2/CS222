83b5b458905d578d003b8b7703f149e9c0b5921a
Joseph Shepin <jshepin@gmail.com>
2023-09-29 00:14:36 -0500

    add ui for seeing multiple users

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import UserProfile from "./components/UserProfile.vue";
+import UsersList from "./components/UsersList.vue";
 </script>
 
 <template>
-  <HelloWorld />
+  <div class="flex justify-center w-full pt-10">
+    <div class="flex w-full gap-4 items-start max-w-5xl">
+      <UsersList />
+      <UserProfile />
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
