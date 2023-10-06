e1a85ccfa111bf522a82c71324f53fb0136e1990
Jaeyun Jeong <jaeyunjeong@MacBook-Pro-552.local>
2023-10-05 20:12:47 -0500

    changed the leetcode endpoint & test.html(test endpoint)

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import requests 
+
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
     return '<h1>Hello from Flask!</h1>'
+
+#github endpoint
+
+@app.route('/github/<username>', methods=['GET'])
+def get_github_user(username):
+    url = f'https://api.github.com/users/{username}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({'error': 'User not found'}), 404
+# leetcode endpoint  
+@app.route('/leetcode/<username>', methods=['GET'])
+def get_leetcode_user(username):
+    url = f'https://leetcodestats.cyclic.app/{username}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({'error': 'User not found'}), 404
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='8080')
