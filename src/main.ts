1b1d9c4c2d6cbae14b8fe63edad2a02b1e387fb6
Joseph Shepin <jshepin@gmail.com>
2023-10-08 14:53:50 -0500

    add configs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { initializeApp } from "firebase/app";
 
-createApp(App).mount('#app')
+/* code from our Firebase console */
+const firebaseConfig = {
+  apiKey: "AIzaSyDqJzb0IyYBYASxvtaws4e7tSHK59iRDSI",
+  authDomain: "uiuc-ad.firebaseapp.com",
+  projectId: "uiuc-ad",
+  storageBucket: "uiuc-ad.appspot.com",
+  messagingSenderId: "280663047030",
+  appId: "1:280663047030:web:421dd3e1b363b406819857",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+createApp(App).mount("#app");
