ede165c38fce46f5c28ecd2654d4e4081b65f1a4
Joseph Shepin <jshepin@gmail.com>
2023-10-13 13:21:25 -0500

    fix changes for ui flow

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,3 @@
-<script setup lang="ts">
-import UserProfile from "../components/UserProfile.vue";
-import UsersList from "../components/UsersList.vue";
-import UserBubble from "../components/UserBubble.vue";
-</script>
 <template>
   <div class="w-full pt-5 px-5">
     <div class="flex justify-center">
@@ -11,10 +6,33 @@ import UserBubble from "../components/UserBubble.vue";
           <UserBubble />
         </div>
         <div class="flex w-full gap-4 mt-3 items-start">
-          <UsersList />
-          <UserProfile />
+          <UsersList @user-selected="userSelected" />
+          <UserProfile
+            @hide-profile="hideProfile"
+            v-show="profileVisible"
+            :user="profUser"
+          />
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import UserProfile from "../components/UserProfile.vue";
+import UsersList from "../components/UsersList.vue";
+import UserBubble from "../components/UserBubble.vue";
+import { ref } from "vue";
+let profileVisible = ref(false);
+const profUser = ref();
+const hideProfile = () => {
+  profileVisible.value = false;
+  console.log(profileVisible);
+};
+const userSelected = (user: any) => {
+  console.log(profileVisible);
+  profileVisible.value = true;
+  profUser.value = user;
+  // console.log(profUser.value.name);
+};
+</script>
