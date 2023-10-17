514efada032062b568f718af2bede6d17d9844dc
Joseph Shepin <jshepin@gmail.com>
2023-10-16 22:27:34 -0500

    add chips in settings

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="w-full border border-gray-200 p-10 rounded-lg overflow-y-auto">
+  <div class="border border-gray-200 p-10 rounded-lg overflow-y-auto">
     <img
       v-if="profileUrl"
       :src="profileUrl"
@@ -37,25 +37,61 @@
           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         />
       </div>
+      <div>
+        <label
+          for="default-input"
+          class="block mb-2 text-sm font-medium text-gray-900"
+          >Classes</label
+        >
+        <div class="mb-6 w-64">
+          <input
+            v-model="classInputValue"
+            v-on:keyup.enter="addClass()"
+            type="text"
+            placeholder="CS 222"
+            id="default-input"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          />
+        </div>
+
+        <div class="flex gap-2 flex-wrap">
+          <uiChip
+            v-for="(item, index) in chipItems"
+            :key="index"
+            :label="item"
+          ></uiChip>
+        </div>
+      </div>
     </div>
-    <uiButton color="blue"> Save </uiButton>
+    <div class="mt-8">
+      <uiButton color="blue"> Save </uiButton>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import uiButton from "../ui/uiButton.vue";
+import uiChip from "../ui/uiChip.vue";
 import { uploadFile } from "@uploadcare/upload-client";
 
 export default {
   components: {
     uiButton,
+    uiChip,
   },
   data() {
     return {
       profileUrl: "",
+      chipItems: ["Tag 1", "Tag 2", "Tag 3"],
+      classInputValue: "",
     };
   },
   methods: {
+    addClass() {
+      this.chipItems.push(this.classInputValue);
+      this.classInputValue = "";
+    },
+
     async uploadFile(fileData: any) {
       console.log("uploading");
       console.log(fileData);
